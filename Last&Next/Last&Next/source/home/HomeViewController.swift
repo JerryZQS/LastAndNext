139153c3483d0a6c0f2f6cddef0f8e52d74579ee
Jerry <jerry.qiushi@gmail.com>
2021-07-23 11:17:54 +0800

    create project

---
+++ Last&Next/Last&Next/source/home/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  Last&Next
+//
+//  Created by Jerry on 2021/7/23.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = R.color.background()
+    }
+    
+}
